abd04f83a3624b8c5b67e8cf8c6f8cfe4af19e45
leesiper <lic0914@163.com>
2024-05-03 21:19:01 +0800

    initial

---
+++ logseqsample/src/main/java/com/leesiper/logseqsample/LogseqsampleApplication.java
@@ -0,0 +1,13 @@
+package com.leesiper.logseqsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogseqsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogseqsampleApplication.class, args);
+    }
+
+}
